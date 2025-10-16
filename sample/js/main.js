3e7c2fa1bf8f1f79e670ebdade7a4cfdb65a7709
ducquan1997 <ducquan1097@gmail.com>
2025-10-16 10:28:24 +0700

    16/10/2025

--- sample/js/main.js
+++ sample/js/main.js
@@ -1,34 +1,24 @@
 $(document).ready(function () {
   // GLOBAL CALL
-  globalHandler();
+  globalHandle();
 
   // PAGE CALL
-  homepageHandler();
-  categoryHandler();
-  productDetailHandler();
-
+  homepageHandle();
+  categoryHandle();
+  productDetailHandle();
 });
 
-
-// 
-function globalHandler() {
-
-}
-
-// 
-function homepageHandler() {
-  
-}
+//
+function globalHandle() {}
 
 //
-function categoryHandler() {
+function homepageHandle() {}
 
-}
+//
+function categoryHandle() {}
 
-// 
-function productDetailHandler() {
-
-}
+//
+function productDetailHandle() {}
 
 let galleryThumbs = new Swiper(".gallery-thumbs", {
   spaceBetween: 4,
@@ -65,25 +55,13 @@ let galleryTop = new Swiper(".gallery-top", {
     slideChange: function () {
       let activeIndex = this.activeIndex + 1;
 
-      let activeSlide = document.querySelector(
-        `.gallery-thumbs .swiper-slide:nth-child(${activeIndex})`
-      );
-      let nextSlide = document.querySelector(
-        `.gallery-thumbs .swiper-slide:nth-child(${activeIndex + 1})`
-      );
-      let prevSlide = document.querySelector(
-        `.gallery-thumbs .swiper-slide:nth-child(${activeIndex - 1})`
-      );
+      let activeSlide = document.querySelector(`.gallery-thumbs .swiper-slide:nth-child(${activeIndex})`);
+      let nextSlide = document.querySelector(`.gallery-thumbs .swiper-slide:nth-child(${activeIndex + 1})`);
+      let prevSlide = document.querySelector(`.gallery-thumbs .swiper-slide:nth-child(${activeIndex - 1})`);
 
-      if (
-        nextSlide &&
-        !nextSlide.classList.contains("swiper-slide-visible")
-      ) {
+      if (nextSlide && !nextSlide.classList.contains("swiper-slide-visible")) {
         this.thumbs.swiper.slideNext();
-      } else if (
-        prevSlide &&
-        !prevSlide.classList.contains("swiper-slide-visible")
-      ) {
+      } else if (prevSlide && !prevSlide.classList.contains("swiper-slide-visible")) {
         this.thumbs.swiper.slidePrev();
       }
     },
@@ -92,6 +70,6 @@ let galleryTop = new Swiper(".gallery-top", {
 
 $('[data-fancybox="gallery"]').fancybox({
   thumbs: {
-    autoStart: true
-  }
+    autoStart: true,
+  },
 });
